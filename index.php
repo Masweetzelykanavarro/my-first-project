87a6e3fb260d14399c8e0c024f29265502b3e850
sweet <masweetzelykanavrro@gmail.com>
2023-11-08 15:42:47 +0800

    commited

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body></body>
+
+<div class="row">
+    <div class="col-4 bg-success"></div>
+    <div class="col-4 bg-warning">
+        <h1 class="display-1 text-center">H1</h1>
+    </div>
+    <div class="col-4 bg-primary"></div>
+   
+    </div>
+
+
+</div>
+
+<div class="row">
+<div class="col-6 bg-secondary">
+    <div class="row m-1"></div>
+<h1 class="display-1 text-center">H2</h1>  
+</div>
+<div class="col-6 bg-danger">
+<div class="row">
+<h1 class="text-center">H1</h1>
+<div class="col-6 bg-primary"></div>
+<div class="col-6 bg dark">
+    <h1 class="text-center">H2</h1>
+</div>
+<div class="col-6 bg-primary"></div>
+</div>    
+</div>
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+<script src="assets/js/jquery-3.7.1.js"></script>
+<script src="assets/js/global.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+</html>
